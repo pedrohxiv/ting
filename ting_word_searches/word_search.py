5c052c13266dce85b1c65f2439db1529da358f1f
Pedro <pedrohalmeidamendonca@gmail.com>
2023-06-14 14:18:28 -0300

    Requisito 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for i in range(len(instance)):
+        file = instance.search(i)
+
+        if any(
+            word.lower() in line.lower()
+            for line in file['linhas_do_arquivo']
+        ):
+            result.append({
+                'palavra': word,
+                'arquivo': file['nome_do_arquivo'],
+                'ocorrencias': [
+                    {'linha': index + 1}
+                    for index, line in enumerate(file['linhas_do_arquivo'])
+                    if word.lower() in line.lower()
+                ]
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
