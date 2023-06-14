cc963bc47138020680396893c642603b31ffb061
Pedro <pedrohalmeidamendonca@gmail.com>
2023-06-13 23:26:00 -0300

    Requisito 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if any(file['nome_do_arquivo'] == path_file for file in instance.queue):
+        return
+
+    lines = txt_importer(path_file)
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(data)
+
+    print(data)
 
 
 def remove(instance):
