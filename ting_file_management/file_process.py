6bb8840a03fa039a4bdd06d595846d260b936bc8
Pedro <pedrohalmeidamendonca@gmail.com>
2023-06-13 23:38:44 -0300

    Requisito 04

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -19,7 +19,11 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    try:
+        file_name = instance.dequeue()['nome_do_arquivo']
+        print(f'Arquivo {file_name} removido com sucesso')
+    except IndexError:
+        print('Não há elementos')
 
 
 def file_metadata(instance, position):
